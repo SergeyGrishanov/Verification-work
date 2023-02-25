12340457ed52d18179636bcfcd6ee466147bbb71
Sergey <blackwhiterainbow@yandex.ru>
2023-02-25 13:11:32 +0300

    Решение задачи через C#

---
+++ TaskProgramm/Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// Пример массива {"458", "1991", "hello", "world", "mas", "science", "47"};
+Console.Clear();
+
+Console.WriteLine("Введите элементы массива через пробел: ");
+string EnterArray = Console.ReadLine()!;
+
+string[] array1 = EnterArray.Split(" "); 
+string[] array2 = new string[array1.Length];
+void ResultArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ResultArray(array1, array2);
+PrintArray(array2);
